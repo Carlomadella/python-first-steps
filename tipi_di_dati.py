f3bc440b3f90034635ea5de523a87b31515c9675
Carlomadella <madella871@gmail.com>
2026-02-17 19:21:43 +0100

    I tipi di dati più importanti

---
+++ tipi_di_dati.py
@@ -0,0 +1,20 @@
+# TIPI DI DATI
+
+# Dati singoli
+# str: x = "ciao"  # stringhe
+# int: x = 5  # numeri interi senza virgola
+# float: x = 2.5  # numeri con la virgola
+# bool: x = True  # può avere come opzioni True o False, sempre con la prima lettera maiuscola
+
+# Collezioni di dati
+# list: x = ["Milano", "Genova", "Palermo"] # una lista di dati, diverso dall'array di JS
+# tuple: x = ("Milano", "Genova", "Palermo")
+# range: x = range(6) # crea una list con i numeri da 1 a 5, equivale a scrivere: x = [0,1,2,3,4,5]
+# dict: x = {"nome": "Milano", "abitanti": 1500000} # dictionary
+# set: x = {"Milano", "Genova", "Palermo"}
+
+# per sapere il tipo di dato:
+x = 14
+
+print(type(x))
+# in console uscirà <class 'int'>, indicà che il dato è di tipo int cioè un numero intero
